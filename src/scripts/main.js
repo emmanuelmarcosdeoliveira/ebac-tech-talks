2834e83aa8f2d13c92434afbee2310a4b66af782
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2023-11-08 17:56:45 -0300

    :sparkles: inclusion of time counter:

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,35 @@
 AOS.init();
+
+const dataDoEvento = new Date("Nov 07, 2023 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+const contaAsHoras = setInterval(function () {
+  const agora = new Date();
+  const timeStampAtual = agora.getTime();
+
+  const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+  const diasEmMs = 1000 * 60 * 60 * 24;
+  const horaEmMs = 1000 * 60 * 60;
+  const minutoEmMs = 1000 * 60;
+
+  const diasAteoEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+  const horasAteOEvento = Math.floor(
+    (distanciaAteOEvento % diasEmMs) / horaEmMs
+  );
+  const minutosAteOEvento = Math.floor(
+    (distanciaAteOEvento % horaEmMs) / minutoEmMs
+  );
+  const segundosAteOEvento = Math.floor(
+    (distanciaAteOEvento % minutoEmMs) / 1000
+  );
+
+  document.getElementById(
+    "contador"
+  ).innerHTML = `${diasAteoEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+  if (distanciaAteOEvento < 0) {
+    clearInterval(contaAsHoras);
+    document.getElementById("contador").innerHTML =
+      "Que pena você perdeu esse  evento! 😥";
+  }
+}, 1000);
